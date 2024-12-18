049c8b47474ef0335470780f0438d4fce2cc8f18
Epithumia <endless@airelle.info>
2024-12-18 08:31:50 +0100

    Optimize day 18

--- 2024/day18.py
+++ 2024/day18.py
@@ -2,6 +2,16 @@ import heapq
 from collections import defaultdict
 from math import inf
 
+
+def rebuild_path(seen, source, destination):
+    path = []
+    current = destination
+    while current != source:
+        path.append(current)
+        current = seen[current]
+    return path
+
+
 def dijkstra(graph, source, destination):
     seen = defaultdict(lambda: False)
     bag = []
@@ -10,11 +20,11 @@ def dijkstra(graph, source, destination):
         distance, current, parent = heapq.heappop(bag)
         if current == destination:
             seen[current] = parent
-            return distance, seen
+            return distance, rebuild_path(seen, source, destination)
         if not seen[current]:
             seen[current] = parent
             neighbors = []
-            for move in [(0,1), (0,-1), (1,0), (-1,0)]:
+            for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
                 v = (current[0] + move[0], current[1] + move[1])
                 if graph[v] != "#":
                     neighbors.append(v)
@@ -23,7 +33,8 @@ def dijkstra(graph, source, destination):
                     bag,
                     (1 + distance, neighbor, current),
                 )
-    return inf, seen
+    return inf, []
+
 
 with open("2024/input/day18") as f:
     data = f.read().splitlines()
@@ -32,18 +43,20 @@ size = 71
 grid = defaultdict(lambda: "#")
 for i in range(size):
     for j in range(size):
-        grid[i,j] = "."
+        grid[i, j] = "."
 
 for i in range(len(data)):
     d = 0
     x = int(data[i].split(",")[0])
     y = int(data[i].split(",")[1])
-    p = (y,x)
+    p = (y, x)
     grid[p] = "#"
-    if i >= 1023:
-        d, s = dijkstra(grid, (0,0), (size-1, size-1))
     if i == 1023:
+        d, s = dijkstra(grid, (0, 0), (size - 1, size - 1))
         print("Part 1:", d)
-    if d == inf:
-        print("Part 2:", f"{p[1]},{p[0]}")
-        break
+    elif i > 1023:
+        if p in s:
+            d, s = dijkstra(grid, (0, 0), (size - 1, size - 1))
+            if d == inf:
+                print("Part 2:", f"{p[1]},{p[0]}")
+                break
