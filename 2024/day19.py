b431f7267008ac2fe63d9ca9c715a382d7383ec8
Epithumia <endless@airelle.info>
2024-12-19 14:30:19 +0100

    Optimize a little

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,22 +1,28 @@
 from functools import cache
 
 
-@cache
 def all_options(target, towels):
-    if len(target) == 0:
-        return 1
-    count = 0
-    for t in towels:
-        if t == target[: len(t)]:
-            count += all_options(target[len(t) :], towels)
-    return count
+
+    @cache
+    def find(target):
+        if len(target) == 0:
+            return 1
+        count = 0
+        for t in towels:
+            if t == target[: len(t)]:
+                count += find(target[len(t) :])
+        return count
+
+    return find(target)
 
 
 with open("2024/input/day19") as f:
     data = f.read().splitlines()
-    towels = tuple(x for x in data[0].split(", "))
+    towels = data[0].split(", ")
     targets = [x for x in data[2:]]
 
-print("Part 1:", sum([1 if all_options(x, tuple(towels)) else 0 for x in targets]))
+options = [all_options(x, towels) for x in targets]
 
-print("Part 2:", sum([all_options(x, tuple(towels)) for x in targets]))
+print("Part 1:", sum(1 for x in options if x > 0))
+
+print("Part 2:", sum(options))
